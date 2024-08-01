b7d962c3728176057aee046af0fd36c733b7006c
silentrald <silentrald1216@gmail.com>
2024-08-01 09:52:04 +0800

    config: added system clipboard configuration

--- lua/silent/set.lua
+++ lua/silent/set.lua
@@ -1,8 +1,8 @@
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.foldmethod = 'expr'
-vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
 vim.opt.foldlevel = 2
 vim.opt.foldnestmax = 10
 
@@ -14,11 +14,11 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.cindent = true
 
-vim.opt.listchars = 'tab:>-,trail:.,lead:.'
+vim.opt.listchars = "tab:>-,trail:.,lead:."
 vim.opt.list = true
 
-vim.opt.encoding = 'UTF-8'
-vim.opt.path = '**'
+vim.opt.encoding = "UTF-8"
+vim.opt.path = "**"
 
 vim.opt.termguicolors = true
 
@@ -26,3 +26,5 @@ vim.opt.scrolloff = 4
 
 vim.opt.updatetime = 50
 
+-- System clipboard config; then either install xclip or wl-clipboard
+vim.opt.clipboard:append("unnamedplus")
