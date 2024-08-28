34da48c9cc090467d38572f0cd320eec1d984f05
silentrald <silentrald1216@gmail.com>
2024-08-28 20:57:01 +0800

    use regex search with telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,6 +3,9 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-q>', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<C-h>', function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ") });
+  builtin.grep_string({
+    use_regex = true,
+    search = vim.fn.input("Grep > ")
+  });
 end)
 
